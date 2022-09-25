248353a27146027f7d26fa8d3e08c3820d9e209d
Laraoldebolhaar <102327321+Laraoldebolhaar@users.noreply.github.com>
2022-09-25 18:56:40 +0200

    Added the search engine

--- src/app.js
+++ src/app.js
@@ -55,9 +55,37 @@ function displayTemperature(response) {
   );
 }
 
-let apiKey = "6672216a4aa17866c4eee801a1995ca5";
-let city = "Amsterdam";
-let apiEndpoint = `https://api.openweathermap.org/data/2.5/weather?`;
-let apiUrl = `${apiEndpoint}q=${city}&appid=${apiKey}&units=metric`;
+function search(city) {
+  let apiKey = "6672216a4aa17866c4eee801a1995ca5";
+  let apiEndpoint = `https://api.openweathermap.org/data/2.5/weather?`;
+  let apiUrl = `${apiEndpoint}q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
 
-axios.get(apiUrl).then(displayTemperature);
+function handelSubmit(event) {
+  event.preventDefault();
+  let cityInput = document.querySelector("#city-input");
+  search(cityInput.value);
+}
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handelSubmit);
+
+let currentLocationButton = document.querySelector("#current-location");
+currentLocationButton.addEventListener("click", navGeoLoc);
+
+function currentLocation(position) {
+  let lat = position.coords.latitude;
+  let lon = position.coords.longitude;
+
+  let apiKey = "6672216a4aa17866c4eee801a1995ca5";
+  let apiEndpoint = "https://api.openweathermap.org/data/2.5/weather?";
+  let apiUrl = `${apiEndpoint}lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
+
+  axios.get(`${apiUrl}&appid=${apiKey}`).then(displayTemperature);
+}
+
+function navGeoLoc(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(currentLocation);
+}

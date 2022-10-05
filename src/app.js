f874e659f9a2fd45086ba8d9b5b7efc5137f0285
Laraoldebolhaar <laraoldebolhaar@gmail.com>
2022-10-05 15:04:43 +0200

    Added the coordinates

--- src/app.js
+++ src/app.js
@@ -1,4 +1,6 @@
-function displayForecast() {
+function displayForecast(response) {
+  console.log(response.data);
+
   let forecastElement = document.querySelector("#forecast");
 
   let forecastHTML = `<div class="row">`;
@@ -8,7 +10,7 @@ function displayForecast() {
       forecastHTML +
       `<div class="col-2">
         <div class="weather-forecast-date">${day}</div>
-          <img src="images\cloud.png" alt="cloud" class="forecast-img" />
+          <img src="" alt="cloud" class="forecast-img" />
             <div class="weather-forecast-temperatures">
               <span class="weather-forecast-temperature-max">18° </span><span class="weather-forecast-temperature-min">12°</span>
             </div>
@@ -16,7 +18,14 @@ function displayForecast() {
   });
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
-  console.log(forecastHTML);
+}
+
+function getForecast(coordinates) {
+  console.log(coordinates);
+  let apiKey = "6672216a4aa17866c4eee801a1995ca5";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(displayForecast);
 }
 
 function displayTemperature(response) {
@@ -45,6 +54,8 @@ function displayTemperature(response) {
     "src",
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
+
+  getForecast(response.data.coord);
 }
 
 function search(city) {
@@ -168,5 +179,3 @@ let months = [
 let month = months[now.getMonth()];
 let currentDateAndTime = document.querySelector("#time-date");
 currentDateAndTime.innerHTML = `${day} ${month} ${date}, ${hours}:${minutes}`;
-
-displayForecast();
